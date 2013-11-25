35e8ef6fbdab9c4607694df822eaef2b6eb73b95
khvalera <khvalera@narod.ru>
2013-11-26 01:06:30 +0300

    v0.0.1

--- ifup-wvdial.sh
+++ ifup-wvdial.sh
@@ -1,11 +1,13 @@
-IP=$1
+remote_ip=$1
 path_log=$2
-wvdialconf="/etc/wvdial.conf"
-[ -z ${IP} ] && IP=8.8.8.8
+wvdial_conf="/etc/wvdial.conf"
+[ -z ${remote_ip} ] && remote_ip=8.8.8.8
 [ -z ${path_log} ] && path_log="/var/log"
 datal=`date +%Y%m%d`
 datat=`date +%T`
-defineOS=`cat /etc/issue | grep 'ClearOS'`
+define_clearos=`cat /etc/issue | grep 'ClearOS'`
+number_cycles_eth=3; cycle_eth=0
+ping_eth="no"; ping_ppp="no"
 
 fix_network_conf() {
   interface=$1
@@ -19,19 +21,19 @@ fix_network_conf() {
 }
 
 fix_wvdial_conf() {
-  if ! [ -f ${wvdialconf} ]; then
-     echo "${datat} - File not found "${wvdialconf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
-     echo "${datat} - Create a new file "${wvdialconf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
-     touch "${wvdialconf}"
+  if ! [ -f ${wvdial_conf} ]; then
+     echo "${datat} - File not found "${wvdial_conf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+     echo "${datat} - Create a new file "${wvdial_conf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+     touch "${wvdial_conf}"
      error_code=$?
      if [ "${error_code}" != "0" ]; then
         echo "Error code ${error_code}"
         exit 1
      fi
   fi
-  numrows=`wc -l "${wvdialconf}" | awk '{print $1}'`
+  numrows=`wc -l "${wvdial_conf}" | awk '{print $1}'`
   if [ "${numrows}" = "0" ]; then
-    echo "${datat} - Generation of the configuration file "${wvdialconf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+    echo "${datat} - Generation of the configuration file "${wvdial_conf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
     wvdialconf
     error_code=$?
     if [ "${error_code}" != "0" ]; then
@@ -39,48 +41,47 @@ fix_wvdial_conf() {
        exit 1
     fi
   fi
-  checkconf=`awk -F "=" '/; Phone =/{print $2}' ${wvdialconf} | sed 's/^[ \t]*//'`
+  checkconf=`awk -F "=" '/; Phone =/{print $2}' ${wvdial_conf} | sed 's/^[ \t]*//'`
   if [ "${checkconf}" = "<Target Phone Number>" ]; then
-    echo "${datat} - Complete the 'Phone' in file "${wvdialconf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+    echo "${datat} - Complete the 'Phone' in file "${wvdial_conf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
     exit 1
   fi
-  checkconf=`awk -F "=" '/; Username =/{print $2}' ${wvdialconf} | sed 's/^[ \t]*//'`
+  checkconf=`awk -F "=" '/; Username =/{print $2}' ${wvdial_conf} | sed 's/^[ \t]*//'`
   if [ "${checkconf}" = "<Your Login Name>" ]; then
-    echo "${datat} - Complete the 'Username' in file "${wvdialconf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+    echo "${datat} - Complete the 'Username' in file "${wvdial_conf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
     exit 1
   fi
-  checkconf=`awk -F "=" '/; Password =/{print $2}' ${wvdialconf} | sed 's/^[ \t]*//'`
+  checkconf=`awk -F "=" '/; Password =/{print $2}' ${wvdial_conf} | sed 's/^[ \t]*//'`
   if [ "${checkconf}" = "<Your Password>" ]; then
-    echo "${datat} - Complete the 'Password' in file "${wvdialconf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+    echo "${datat} - Complete the 'Password' in file "${wvdial_conf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
     exit 1
   fi
-  checkconf=`awk -F "=" '/Auto dns =/{print $2}' ${wvdialconf} | sed 's/^[ \t]*//'`
+  checkconf=`awk -F "=" '/Auto dns =/{print $2}' ${wvdial_conf} | sed 's/^[ \t]*//'`
   if [ -z "${checkconf}" ]; then
-    sed -i '/Password/ i\Auto dns = 1' ${wvdialconf}
-    echo "${datat} - Added option 'Auto dns' in file "${wvdialconf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+    sed -i '/Password/ i\Auto dns = 1' ${wvdial_conf}
+    echo "${datat} - Added option 'Auto dns' in file "${wvdial_conf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
   fi
-  checkconf=`awk -F "=" '/Dial Command =/{print $2}' ${wvdialconf} | sed 's/^[ \t]*//'`
+  checkconf=`awk -F "=" '/Dial Command =/{print $2}' ${wvdial_conf} | sed 's/^[ \t]*//'`
   if [ -z "${checkconf}" ]; then
-    sed -i '/Password/ i\Dial Command = ATDT' ${wvdialconf}
-    echo "${datat} - Added option 'Dial Command' in file "${wvdialconf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+    sed -i '/Password/ i\Dial Command = ATDT' ${wvdial_conf}
+    echo "${datat} - Added option 'Dial Command' in file "${wvdial_conf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
   fi
-  checkconf=`awk -F "=" '/Stupid Mode =/{print $2}' ${wvdialconf} | sed 's/^[ \t]*//'`
+  checkconf=`awk -F "=" '/Stupid Mode =/{print $2}' ${wvdial_conf} | sed 's/^[ \t]*//'`
   if [ -z "${checkconf}" ]; then
-    sed -i '/Password/ i\Stupid Mode = 1' ${wvdialconf}
-    echo "${datat} - Added option 'Stupid Mode' in file "${wvdialconf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+    sed -i '/Password/ i\Stupid Mode = 1' ${wvdial_conf}
+    echo "${datat} - Added option 'Stupid Mode' in file "${wvdial_conf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
   fi
-  checkconf=`awk -F "=" '/Baud =/{print $2}' ${wvdialconf} | sed 's/^[ \t]*//'`
+  checkconf=`awk -F "=" '/Baud =/{print $2}' ${wvdial_conf} | sed 's/^[ \t]*//'`
   if [[ "${checkconf}" -ne "921600" ]]; then
-    sed -i 's|Baud.*|Baud = 921600|g' ${wvdialconf}
-    echo "${datat} - Fixed option 'Baud' in file "${wvdialconf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+    sed -i 's|Baud.*|Baud = 921600|g' ${wvdial_conf}
+    echo "${datat} - Fixed option 'Baud' in file "${wvdial_conf} 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
   fi
 
 }
 
 create_ifcfg_ppp0(){
- if [ -n "${defineOS}" ]; then
-   ifcfgppp0=/etc/sysconfig/network-scripts/ifcfg-ppp0
-   if ! [ -f "${ifcfgppp0}" ]; then
+  ifcfgppp0=/etc/sysconfig/network-scripts/ifcfg-ppp0
+  if ! [ -f "${ifcfgppp0}" ]; then
       echo 'DEVICE=ppp0
            NAME=DSLppp0
            TYPE="dialup"
@@ -97,38 +98,209 @@ create_ifcfg_ppp0(){
            DEMAND=no
            IDLETIMEOUT=600' >> "${ifcfgppp0}"
       sed -i 's/^[ \t]*//' "${ifcfgppp0}"
-      sed -i "s|MODEMPORT=|MODEMPORT="${usbdev}"|g" "${ifcfgppp0}"
+      sed -i "s|MODEMPORT=|MODEMPORT="${usb_dev}"|g" "${ifcfgppp0}"
       echo "${datat} - Create a file ${ifcfgppp0}" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
-   fi
- fi
+  fi
 }
 
-add_default_route(){
- echo "!!!!!!"
- gateway_dev=`ip route list | awk '/^default / { print $3 }'`
- if [ -f /var/run/ppp0.pid ] ; then
-    if [ "${gateway_dev}" != "ppp0" ]; then
-         DEFRT=$(ip route list match 0.0.0.0/0)
-         echo "$DEFRT" | while read spec; do
-           ip route del $spec;
-         done
-     gateway=`ip route list | grep "ppp0  proto kernel" | awk '{ print $1 }'`
-     ip route add default via "${gateway}" dev ppp0
-   fi
- else
+del_default_route(){
+  echo "${datat} - Remove the default route" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+  DEFRT=$(ip route list match 0.0.0.0/0)
+  echo "$DEFRT" | \
+  while read spec; do
+    if [ -n "${spec}" ]; then
+       ip route del $spec;
+    fi
+  done
+}
+
+change_default_route(){
+  echo "${datat} - Change default route" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+  gateway_def=`ip route list | awk '/^default / { print $3 }'`
+  gateway_dev=`ip route list | awk '/^default / { print $5 }'`
+  if [ -f /var/run/ppp0.pid ] ; then
+     if [ "${gateway_dev}" != "ppp0" ]; then
+        wvdial_found="no"
+        ps -A | grep 'wvdial' > /dev/null 2>&1 && wvdial_found="yes"
+        echo "${datat} - Found running WvDial ${wvdial_found}" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+        if [ "${wvdial_found}" = "no" ]; then
+           echo "${datat} - del /var/run/ppp0.pid"
+           rm -f /var/run/ppp0.pid
+        fi
+        del_default_route
+        gateway=`ip route list | grep "ppp0  proto kernel" | awk '{ print $1 }'`
+        if [ "${gateway}" != "${gateway_def}" ]; then
+           echo "${datat} - Adding a default route gateway=${gateway} for ppp0" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+           ip route add default via "${gateway}" dev ppp0
+           sleep 1
+           echo "${datat} - Restart firewall " 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+           /etc/init.d/firewall restart
+        fi
+     fi
+  else
+    eth0_raised=`ip link show eth0 | grep "UP" | awk '{print $3}' | cut -d ',' -f 4 | cut -d '>' -f 1`
+    if [ "${eth0_raised}" != "LOWER_UP" ]; then
+      return
+    fi
     if [ "${gateway_dev}" != "eth0" ]; then
-      DEFRT=$(ip route list match 0.0.0.0/0)
-      echo "$DEFRT" | while read spec; do
-        ip route del $spec;
-      done
+       del_default_route
       . /etc/sysconfig/network-scripts/ifcfg-eth0
-      gateway=`echo $GATEWAY | awk '{ print $1 }'`
-      ip route add default via "${gateway}" dev eth0
+      if [ ${BOOTPROTO} = "dhcp" ]; then
+         if [ -f /var/lib/dhclient/eth0.routers ]; then
+            gateway=`head -n1 /var/lib/dhclient/eth0.routers`
+        else
+           echo "${datat} - File not found /var/lib/dhclient/eth0.routers" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+           return
+        fi
+      else
+         gateway=`echo $GATEWAY | awk '{ print $1 }'`
+         if [ -z ${gateway}]; then
+            echo "${datat} - Empty parameter GATEWAY for file /etc/sysconfig/network-scripts/ifcfg-eth0" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+            return
+         fi
+      fi
+      if [ "${gateway}" != "${gateway_def}" ]; then
+         echo "${datat} - Adding a default route gateway=${gateway} for eth0" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+         ip route add default via "${gateway}" dev eth0
+         sleep 1
+         echo "${datat} - Restart firewall " 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+         /etc/init.d/firewall restart
+      fi
     fi
  fi
 }
 
-if [ -z "${defineOS}" ]; then
+killall_wvdial(){
+  echo "${datat} - Kill wvdial" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+  killall wvdial 2>/dev/null
+  while true; do
+    sleep 2
+    wvdial_found="no"
+    ps -A | grep 'wvdial' > /dev/null 2>&1 && wvdial_found="yes"
+    if [ ${wvdial_found} = "no" ]; then
+      return
+    fi
+  done
+  if [ -f /var/run/ppp0.pid ]; then
+     echo "${datat} - del /var/run/ppp0.pid" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+     rm -f /var/run/ppp0.pid
+  fi
+}
+
+start_wvdial(){
+  echo "${datat} - Start wvdial" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+  wvdial &
+  while true; do
+    sleep 2
+    wvdial_found="no"
+    ps -A | grep 'wvdial' > /dev/null 2>&1 && wvdial_found="yes"
+    if [ ${wvdial_found} = "no" ]; then
+      return 1
+    fi
+    if [ -f /var/run/ppp0.pid ]; then
+     break
+    fi
+  done
+  sleep 2
+  return 0
+}
+
+checking_internet() {
+  gateway_dev=`ip route list | awk '/^default / { print $5 }'`
+  if [ -z "${gateway_dev}" ]; then
+     change_default_route
+     gateway_dev=`ip route list | awk '/^default / { print $5 }'`
+  fi
+  if [ "${gateway_dev}" = "ppp0" ]; then
+     ping_ppp="no"
+     /bin/ping -q -I ppp0 ${remote_ip} -c 2 > /dev/null 2>&1 && ping_ppp="yes"
+     echo "${datat} - Ping server via ppp0 " ${ping_ppp}
+     if [ ${ping_ppp} = "no" ]; then
+        usb_found="no"
+        ls -l ${usb_dev} > /dev/null 2>&1 && usb_found="yes"
+        echo "${datat} - Modem was found " ${usb_found}
+        if [ ${usb_found} = "yes" ]; then
+           wvdial_found="no"
+           ps -A | grep 'wvdial' > /dev/null 2>&1 && wvdial_found="yes"
+           echo "${datat} - Found running WvDial ${wvdial_found}"
+           if [ "${wvdial_found}" = "yes" ]; then
+              killall_wvdial
+           fi
+           start_wvdial
+           change_default_route
+        fi
+     else
+        if [ ${number_cycles_eth} = ${cycle_eth} ]; then
+           echo "Check appears on the interface eth0 ( cycle - ${cycle_eth})"
+           cycle_eth=0;
+           eth0_raised=`ip link show eth0 | grep "UP" | awk '{print $3}' | cut -d ',' -f 4 | cut -d '>' -f 1`
+           if [ "${eth0_raised}" = "LOWER_UP" ]; then
+              del_default_route
+              . /etc/sysconfig/network-scripts/ifcfg-eth0
+              if [ ${BOOTPROTO} = "dhcp" ]; then
+                 if [ -f /var/lib/dhclient/eth0.routers ]; then
+                    gateway=`head -n1 /var/lib/dhclient/eth0.routers`
+                 else
+                    echo "${datat} - File not found /var/lib/dhclient/eth0.routers" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+                    return
+                 fi
+              else
+                 gateway=`echo $GATEWAY | awk '{ print $1 }'`
+                 if [ -z ${gateway}]; then
+                    echo "${datat} - Empty parameter GATEWAY for file /etc/sysconfig/network-scripts/ifcfg-eth0" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+                    return
+                 fi
+              fi
+              if [ "${gateway}" != "${gateway_def}" ]; then
+                 echo "${datat} - Adding a default route gateway=${gateway} for eth0" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+                 ip route add default via "${gateway}" dev eth0
+              fi
+              sleep 1
+              ping_eth="no"
+              /bin/ping -q -I eth0 ${remote_ip} -c 2 > /dev/null 2>&1 && ping_eth="yes"
+              echo "${datat} - Ping server via eth0 " ${ping_eth}
+              if [ ${ping_eth} = "no" ]; then
+                 del_default_route
+                 change_default_route
+              else
+                 killall_wvdial
+              fi
+           fi
+        fi
+        let "cycle_eth = ${cycle_eth} + 1";
+     fi
+  else
+     ping_eth="no"
+     /bin/ping -q -I eth0 ${remote_ip} -c 2 > /dev/null 2>&1 && ping_eth="yes"
+     echo "${datat} - Ping server via eth0 " ${ping_eth}
+     if [ ${ping_eth} = "no" ]; then
+        usb_found="no"
+        ls -l ${usb_dev} > /dev/null 2>&1 && usb_found="yes"
+        echo "${datat} - Modem was found " ${usb_found}
+        if [ ${usb_found} = "yes" ]; then
+           wvdial_found="no"
+           ps -A | grep 'wvdial' > /dev/null 2>&1 && wvdial_found="yes"
+           echo "${datat} - Found running WvDial ${wvdial_found}" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
+           if [ "${wvdial_found}" = "yes" ]; then
+              change_default_route
+              sleep 1
+              ping_ppp="no"
+              /bin/ping -q -I ppp0 ${remote_ip} -c 1 > /dev/null 2>&1 && ping_ppp="yes"
+              if [ ${ping_ppp} = "no" ]; then
+                 killall_wvdial
+              else
+                return
+              fi
+           fi
+           start_wvdial
+           change_default_route
+        fi
+     fi
+  fi
+}
+
+
+if [ -z "${define_clearos}" ]; then
   echo "Script is designed to ClearOS"
   exit
 fi
@@ -140,9 +312,9 @@ if [ "${error_code}" != "0" ]; then
    exit 1
 fi
 
-usbdev=`awk -F "=" '/^Modem =/{print $2}' ${wvdialconf} | sed 's/^[ \t]*//'`
-[ -z "${usbdev}" ] && usbdev=`awk -F "=" '/^Modem=/{print $2}' ${wvdialconf}`
-if [ -z "${usbdev}" ]; then
+usb_dev=`awk -F "=" '/^Modem =/{print $2}' ${wvdial_conf} | sed 's/^[ \t]*//'`
+[ -z "${usb_dev}" ] && usb_dev=`awk -F "=" '/^Modem=/{print $2}' ${wvdial_conf}`
+if [ -z "${usb_dev}" ]; then
   echo "${datat} - Not found option 'Modem'" 2>&1 | tee -a ${path_log}"/ifup-wvdial.log"
   exit 1
 fi
@@ -150,44 +322,6 @@ fi
 while true; do
   fix_network_conf "ppp0"
   sleep 3
-  ping1="no"
-  ping2="no"
   datat=`date +%T`
-  /bin/ping -I eth0 ${IP} -c 2 > /dev/null 2>&1 && ping1="yes"
-  /bin/ping -I ppp0 ${IP} -c 2 > /dev/null 2>&1 && ping2="yes"
-  echo "${datat} - Ping server via eth0 " ${ping1}
-  echo "${datat} - Ping server via ppp0 " ${ping2}
-  usbfound="no"
-  ls -l ${usbdev} > /dev/null 2>&1 && usbfound="yes"
-  echo "${datat} - Modem was found " ${usbfound}
-  fiundwvdial="no"
-  ps -A | grep 'wvdial' > /dev/null 2>&1 && fiundwvdial="yes"
-  echo "${datat} - Fiund was wvdial ${fiundwvdial}"
-  if [ ${ping1} = "no" -a ${ping2} = "no" ]; then
-    if [ ${usbfound} = "yes" -a "${fiundwvdial}" = "no" ]; then
-        echo "${datat} - Start wvdial"
-        wvdial &
-        error_code=$?
-        if [ "${error_code}" != "0" ]; then
-            echo "Error code ${error_code}"
-        fi
-        sleep 10
-        add_default_route
-    else
-      if [ "${fiundwvdial}" = "yes" ]; then
-        echo "${datat} - Kill wvdial"
-        killall wvdial 2>/dev/null
-        sleep 5
-        add_default_route
-      fi
-    fi
-  fi
-  if [ ${ping1} = "yes" -a ${ping2} = "no" ]; then
-    if [ ${usbfound} = "yes" -a "${fiundwvdial}" = "yes" ]; then
-       echo "${datat} - Kill wvdial"
-       killall wvdial 2>/dev/null
-       sleep 5
-       add_default_route
-    fi
-  fi
+  checking_internet
 done
